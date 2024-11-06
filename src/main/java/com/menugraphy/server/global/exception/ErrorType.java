bb984b32854e9c74f8181ee69cde213052db45ea
ckkim817 <ckkim817@konkuk.ac.kr>
2024-11-06 17:05:43 +0900

    setting: 커스텀 예외 처리 구현 (#1)

---
+++ src/main/java/com/menugraphy/server/global/exception/ErrorType.java
@@ -0,0 +1,23 @@
+package com.menugraphy.server.global.exception;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public enum ErrorType {
+
+    INVALID_REQUEST_ERROR(HttpStatus.BAD_REQUEST, "40001", "잘못된 요청입니다."),
+    TYPE_MISMATCH_ERROR(HttpStatus.BAD_REQUEST, "40002", "잘못된 값이 입력되었습니다."),
+    INVALID_REQUEST_BODY_ERROR(HttpStatus.BAD_REQUEST, "40003", "잘못된 Request Body입니다. 요청 형식 또는 필드를 확인하세요."),
+    DATA_INTEGRITY_VIOLATION_ERROR(HttpStatus.BAD_REQUEST, "40004", "데이터 무결성 제약 조건을 위반했습니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "50001", "예상치 못한 서버 오류가 발생했습니다."),
+
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
