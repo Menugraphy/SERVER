c8ee47748b45a516da005e8192670135ae14252e
ckkim817 <ckkim817@konkuk.ac.kr>
2024-11-08 17:19:33 +0900

    feat: Category, Type 엔티티 작성 (#3)

---
+++ src/main/java/com/menugraphy/server/domain/food/model/entity/Category.java
@@ -0,0 +1,26 @@
+package com.menugraphy.server.domain.food.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
