8c7da87cccb29ae955fe6a185956754a09c3be98
ckkim817 <ckkim817@konkuk.ac.kr>
2024-11-08 16:09:33 +0900

    feat: Like 엔티티 작성 (#3)

---
+++ src/main/java/com/menugraphy/server/domain/member/model/entity/Like.java
@@ -0,0 +1,39 @@
+package com.menugraphy.server.domain.member.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "like")
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "member_id", nullable = false)
+    private long memberId;
+
+    @Column(name = "food_id", nullable = false)
+    private long foodId;
+
+    @Builder
+    private Like(
+            final long memberId,
+            final long foodId
+    ) {
+        this.memberId = memberId;
+        this.foodId = foodId;
+    }
+}
